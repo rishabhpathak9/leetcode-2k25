cc6f5bca5f00c6671397c7ca18a798147d5c95cb
rishabh <rp.rishabhpathak@gmail.com>
2025-09-21 17:15:23 +0530

    Fast Slow Pointers | 141. Linked List Cycle

---
+++ src/main/java/com/zenith/ListNode.java
@@ -0,0 +1,32 @@
+package com.zenith;
+
+public class ListNode{
+    int val;
+    ListNode next;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    public ListNode(int x) { val = x; }
+
+    public static ListNode createSampleLinkedList() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        return head;
+    }
+}
---
+++ src/main/java/com/zenith/something/fastSlowPointers/LinkedListCycle.java
@@ -0,0 +1,42 @@
+package com.zenith.something.fastSlowPointers;
+
+import java.util.Objects;
+
+import com.zenith.ListNode;
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (Objects.nonNull(fast) && Objects.nonNull(fast.getNext())) {
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Method to create a sample linked list with a cycle
+    public static ListNode createSampleLinkedListWithCycle() {
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        head.setNext(second);
+        second.setNext(third);
+        third.setNext(fourth);
+        fourth.setNext(second);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = createSampleLinkedListWithCycle();
+        LinkedListCycle detector = new LinkedListCycle();
+        boolean hasCycle = detector.hasCycle(head);
+        System.out.println("Linked list has cycle: " + hasCycle);
+    }
+}
