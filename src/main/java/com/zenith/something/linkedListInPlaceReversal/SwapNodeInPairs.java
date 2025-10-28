e6d134573d2ffbd7865aa9703fdd91a9c28f6d2e
rishabh <rp.rishabhpathak@gmail.com>
2025-10-28 22:12:32 +0530

    Reverse LL | 24. Swap Nodes in Pairs

---
+++ src/main/java/com/zenith/something/linkedListInPlaceReversal/SwapNodeInPairs.java
@@ -0,0 +1,42 @@
+package com.zenith.something.linkedListInPlaceReversal;
+
+import java.util.Objects;
+
+import com.zenith.ListNode;
+import static com.zenith.ListNode.printList;
+
+public class SwapNodeInPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode prev = head;
+        if(head == null || head.getNext() == null){
+            return head;
+        }
+        ListNode toReturn = head.getNext();
+        ListNode curr;
+        ListNode next;
+        ListNode lastPrev = null;
+        while(prev != null && prev.getNext() != null){
+            curr = prev.getNext();
+            next = curr.getNext(); 
+            curr.setNext(prev);
+            prev.setNext(next);
+            if(lastPrev!= null){
+                lastPrev.setNext(curr);
+            }
+            lastPrev = prev;
+            prev = next;
+        }
+        return toReturn;
+    }
+    
+    public static void main(String[] args) {
+        SwapNodeInPairs swapNodeInPairs = new SwapNodeInPairs();
+        ListNode head = ListNode.createSampleLinkedList();
+        printList(head);
+        head = swapNodeInPairs.swapPairs(head);
+        printList(head);
+
+    }
+
+}
