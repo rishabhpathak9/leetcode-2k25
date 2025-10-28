2930c3e4c2896aba68b8cce0d9f193e8e69ecea1
rishabh <rp.rishabhpathak@gmail.com>
2025-10-28 21:22:01 +0530

    Reverse LL | 92. Reverse Linked List II

--- src/main/java/com/zenith/ListNode.java
+++ src/main/java/com/zenith/ListNode.java
@@ -35,6 +35,7 @@ public class ListNode {
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
         return head;
     }
 
---
+++ src/main/java/com/zenith/something/linkedListInPlaceReversal/ReverseLinkedListII.java
@@ -0,0 +1,46 @@
+package com.zenith.something.linkedListInPlaceReversal;
+
+import java.util.Objects;
+
+import com.zenith.ListNode;
+import static com.zenith.ListNode.printList;
+
+public class ReverseLinkedListII {
+
+    public ListNode reverseList2(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.setNext(head);
+        ListNode curr = dummyNode;
+        ListNode beforeLeft = curr;
+        int i = 0;
+        while(i<left){
+            beforeLeft = curr;
+            curr = curr.getNext();
+            i++;
+        }
+        ListNode leftNode = curr;
+        ListNode prev = curr;
+        curr = curr.getNext();
+        ListNode next = null;
+        while(i<right){
+            next = curr.getNext();
+            curr.setNext(prev);
+            prev = curr;
+            curr = next;
+            i++;
+        }            
+        leftNode.setNext(next);
+        beforeLeft.setNext(prev);
+        return dummyNode.getNext();
+    }
+    
+    public static void main(String[] args) {
+        ReverseLinkedListII reverseLinkedList = new ReverseLinkedListII();
+        ListNode head = ListNode.createSampleLinkedList();
+        printList(head);
+        head = reverseLinkedList.reverseList2(head, 2, 4);
+        printList(head);
+
+    }
+
+}
