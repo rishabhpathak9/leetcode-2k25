39b92175e18b988188cec3976778c8e2f3b6f7f2
rishabh <rp.rishabhpathak@gmail.com>
2025-12-13 21:49:35 +0530

    TopKElements | 215. Kth Largest Element in an Array

---
+++ src/main/java/com/zenith/something/topKElements/KthLargestElement.java
@@ -0,0 +1,25 @@
+package com.zenith.something.topKElements;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        int result = findKthLargest(nums, k);
+        System.out.println("Kth largest element: " + result);
+    }
+
+    private static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int val : nums) {
+            heap.add(val);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        return heap.poll();
+    }
+
+}
