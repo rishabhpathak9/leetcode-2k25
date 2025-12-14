4f246393550e9276ef5c2934093ddb0cb5407adc
rishabh <rp.rishabhpathak@gmail.com>
2025-12-15 00:56:34 +0530

    TopKElements | 373. Find K Pairs with Smallest Sums

--- src/main/java/com/zenith/something/topKElements/KPairWithSmallestSums.java
+++ src/main/java/com/zenith/something/topKElements/KPairWithSmallestSums.java
@@ -1,8 +1,11 @@
 package com.zenith.something.topKElements;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class KPairWithSmallestSums {
 
@@ -17,16 +20,26 @@ public class KPairWithSmallestSums {
     }
 
     private static List<List<Integer>> pairWithSmallestSums(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<List<Integer>> heap = new PriorityQueue<>(Comparator.comparingInt(a -> -1 * (a.get(0) + a.get(1))));
-        for (int el1 : nums1) {
-            for (int el2 : nums2) {
-                heap.add(List.of(el1, el2));
-                if (heap.size() > k) {
-                    heap.poll();
-                }
+        PriorityQueue<int[]> heap = new PriorityQueue<>(Comparator.comparingInt(a -> nums1[a[0]] + nums2[a[1]]));
+        Set<String> visited = new HashSet<>();
+        heap.add(new int[]{0, 0});
+        visited.add("0#0");
+        List<List<Integer>> result = new ArrayList<>();
+        while (k-- > 0 && !heap.isEmpty()) {
+            int[] smallestPair = heap.poll();
+            int i = smallestPair[0];
+            int j = smallestPair[1];
+
+            result.add(List.of(nums1[i], nums2[j]));
+            if (i + 1 < nums1.length && !visited.contains((i + 1) + "#" + j)) {
+                heap.add(new int[]{i + 1, j});
+                visited.add((i + 1) + "#" + j);
+            }
+            if (j + 1 < nums2.length && !visited.contains(i + "#" + (j + 1))) {
+                visited.add(i + "#" + (j + 1));
+                heap.add(new int[]{i, j + 1});
             }
         }
-        return heap.stream().toList();
+        return result;
     }
-
 }
