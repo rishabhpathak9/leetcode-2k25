7a5d46793ece74d074363d93e00f446d00482437
rishabh <rp.rishabhpathak@gmail.com>
2025-12-21 00:52:53 +0530

    Modified Binary Search | 240. Search a 2D Matrix II

---
+++ src/main/java/com/zenith/something/modifiedBinarySearch/Search2dMatrixII.java
@@ -0,0 +1,36 @@
+package com.zenith.something.modifiedBinarySearch;
+
+public class Search2dMatrixII {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        
+        int row = 0;
+        int col = matrix[0].length - 1;
+        
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 4, 7, 11},
+            {2, 5, 8, 12},
+            {3, 6, 9, 16},
+            {10, 13, 14, 17}
+        };
+        
+        int target = 5;
+        boolean result = searchMatrix(matrix, target);
+        System.out.println("Found: " + result);
+    }
+}
