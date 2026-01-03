f018f313e0773ad40e3a990449cd2796ee2566f8
rishabh <rp.rishabhpathak@gmail.com>
2026-01-03 20:27:10 +0530

    Matrix Trav | 130. Surrounded Regions

--- src/main/java/com/zenith/something/matricTraversal/SurroundedRegions.java
+++ src/main/java/com/zenith/something/matricTraversal/SurroundedRegions.java
@@ -52,13 +52,12 @@ public class SurroundedRegions {
             }
         }
         
-        for(int i = 0; i<board.length; i++){
-            for(int j = 0; j<board[0].length; j++){
-                if(board[i][j] == 'S'){
-                    board[i][j] = 'O';
-                }
-                else if(board[i][j] == 'O'){
-                    board[i][j] = 'X';
+        for (char[] board1 : board) {
+            for (int j = 0; j<board[0].length; j++) {
+                if (board1[j] == 'S') {
+                    board1[j] = 'O';
+                } else if (board1[j] == 'O') {
+                    board1[j] = 'X';
                 }
             }
         }
